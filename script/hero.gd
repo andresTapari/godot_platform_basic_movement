4468857be62ac5ad421dde0dded6ad149ddd5956
andres <andres@DESKTOP-H444DK3.local>
2022-06-19 10:59:56 -0300

    Actualizar jump outline.png.import, landing outline.png.import y 19 más archivos...

--- script/hero.gd
+++ script/hero.gd
@@ -1,16 +1,38 @@
 extends KinematicBody2D
 
-onready var sprite = get_node('animacion')
+onready var sprite 	 = get_node("AnimatedSprite")
+onready var collider = get_node("CollisionShape")
 
+export (int) var speed = 200
 
-var vel: Vector2 = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
+var contador: int = 0
 
-func _ready() -> void:
-	print("Hola")
+# Esta función se repite
+func _physics_process(_delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+	contador += 1
 	
-func _process(_lamba) -> void:
-	input()
-	pass
+# Función para detectar entradas de teclado.	
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		sprite.play("run")
+		sprite.flip_h = false
 
-func input() -> void:
-	pass
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		sprite.play("run")
+		sprite.flip_h = true
+
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity == Vector2(0,0):
+		sprite.play("idle")	
+	velocity = velocity.normalized() * speed
